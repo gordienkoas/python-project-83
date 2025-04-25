050177fcd9ea0c70368b9593e2c25dcb22aa98e9
user <gordienkoas@bk.ru>
2025-04-25 13:15:11 +0000

    step 1 done.

---
+++ page_analyzer/app.py
@@ -0,0 +1,16 @@
+import os
+from flask import Flask
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
+@app.route('/')
+def index():
+    return "Hello, World!"
+
+__all__ = ['app']
+if __name__ == '__main__':
+    app.run()
