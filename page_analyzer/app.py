9073adb568cb89f14d153ec9c80b5b6423c1910f
user <gordienkoas@bk.ru>
2025-04-29 06:52:42 +0000

    step 2 done.

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -9,7 +9,15 @@ app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 @app.route('/')
 def index():
-    return "Hello, World!"
+    return render_template('index.html')
+
+@app.route('/urls')
+def all_urls():
+    pass
+
+@app.route('/urls', methods=['POST'])
+def add_url():
+    pass
 
 __all__ = ['app']
 if __name__ == '__main__':
